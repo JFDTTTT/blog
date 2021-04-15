19de9523f83ab7a4e9f0ee1a51b48521a9c2237a
jiangfeng <lucyjiangfeng@didiglobal.com>
2021-04-15 20:31:48 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  title: "姜风的博客啊",
+  description: "巴拉巴拉巴拉",
+  themeConfig: {
+    nav: [
+      { text: "主页", link: "/" },
+      {
+        text: "博文",
+        items: [
+          { text: "Android", link: "/android/" },
+          { text: "ios", link: "/ios/" },
+          { text: "Web", link: "/web/" },
+        ],
+      },
+      { text: "关于", link: "/about/" },
+      { text: "Github", link: "https://www.github.com/codeteenager" },
+    ],
+    sidebar: [
+      {
+        title: "Group 1", // 必要的
+        sidebarDepth: 1, // 可选的, 默认值是 1
+        children: ["/"],
+      },
+    ],
+    lastUpdated: "Last Updated",
+  },
+};
