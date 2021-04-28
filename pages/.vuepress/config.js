c39ce9d79a9ba9b840fd98afc3b8067b5c85bdff
jiangfeng <lucyjiangfeng@didiglobal.com>
2021-04-29 00:55:03 +0800

    init

--- pages/.vuepress/config.js
+++ pages/.vuepress/config.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 module.exports = {
   base: '/blog/',
   title: "姜风的博客啊",
@@ -5,15 +7,6 @@ module.exports = {
   themeConfig: {
     nav: [
       { text: "主页", link: "/" },
-      // {
-      //   text: "博文",
-      //   items: [
-      //     { text: "Android", link: "/android/" },
-      //     { text: "ios", link: "/ios/" },
-      //     { text: "Web", link: "/web/" },
-      //   ],
-      // },
-      // { text: "关于", link: "/about/" },
       { text: "Github", link: "https://github.com/JFDTTTT" },
     ],
     sidebar: [
