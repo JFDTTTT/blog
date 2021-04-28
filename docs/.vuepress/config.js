dbad6e9e0f9f3fccfa873ebca14d3f2bb666ae44
jiangfeng <lucyjiangfeng@didiglobal.com>
2021-04-28 15:13:56 +0800

    deploy

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,25 +1,26 @@
 module.exports = {
+  base: '/blog/',
   title: "姜风的博客啊",
   description: "巴拉巴拉巴拉",
   themeConfig: {
     nav: [
       { text: "主页", link: "/" },
-      {
-        text: "博文",
-        items: [
-          { text: "Android", link: "/android/" },
-          { text: "ios", link: "/ios/" },
-          { text: "Web", link: "/web/" },
-        ],
-      },
-      { text: "关于", link: "/about/" },
-      { text: "Github", link: "https://www.github.com/codeteenager" },
+      // {
+      //   text: "博文",
+      //   items: [
+      //     { text: "Android", link: "/android/" },
+      //     { text: "ios", link: "/ios/" },
+      //     { text: "Web", link: "/web/" },
+      //   ],
+      // },
+      // { text: "关于", link: "/about/" },
+      { text: "Github", link: "https://github.com/JFDTTTT" },
     ],
     sidebar: [
       {
-        title: "Group 1", // 必要的
+        title: "TS", // 必要的
         sidebarDepth: 1, // 可选的, 默认值是 1
-        children: ["/"],
+        children: ["/ts/base"],
       },
     ],
     lastUpdated: "Last Updated",
